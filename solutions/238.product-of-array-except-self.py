a904931210033cd20bf6b485ce6ed71671de2949
Min <jungm8453@gmail.com>
2026-07-28 14:14:25 +0000

    260728-1

--- solutions/238.product-of-array-except-self.py
+++ solutions/238.product-of-array-except-self.py
@@ -7,6 +7,18 @@
 # @lc code=start
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-# @lc code=end
+        n = len(nums)
+        res = [0]*n
 
+        pre = 1
+        for i in range(n):
+            res[i] = pre
+            pre *= nums[i]
+
+        suf = 1
+        for i in reversed(range(n)):
+            res[i] *= suf
+            suf *= nums[i]
+
+        return res
+        # @lc code=end
