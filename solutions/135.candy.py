c15b18d262a84ad654e332abb5cbd85ab8e21e96
Min <jungm8453@gmail.com>
2026-07-29 13:20:50 +0000

    leetcode_135

--- solutions/135.candy.py
+++ solutions/135.candy.py
@@ -7,10 +7,20 @@
 # @lc code=start
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        n=len(ratings)
-        res=[1]*n
+        n = len(ratings)
+        res = [1]*n
         for i in range(1, n):
-            
-        
-# @lc code=end
+            if ratings[i-1] > ratings[i]:
+                res[i-1] = res[i]+1
+            if ratings[i-1] < ratings[i]:
+                res[i] = res[i-1]+1
 
+        for i in reversed(range(n-1)):
+            if ratings[i+1] > ratings[i] and res[i+1] <= res[i]:
+                res[i+1] = res[i]+1
+            if ratings[i+1] < ratings[i] and res[i+1] >= res[i]:
+                res[i] = res[i+1]+1
+        return sum(res)
+
+
+# @lc code=end
