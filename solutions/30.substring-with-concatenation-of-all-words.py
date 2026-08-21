cca22730ce314a8f067642aa133820d045b5f8f1
Min <jungm8453@gmail.com>
2026-08-21 13:15:29 +0000

    leetcode-30

--- solutions/30.substring-with-concatenation-of-all-words.py
+++ solutions/30.substring-with-concatenation-of-all-words.py
@@ -7,23 +7,52 @@
 # @lc code=start
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        n=len(words[0])
-        wordSet=set(words)
+        if not s or not words:
+            return []
 
-        for i in range(len(s)):
-            tempSet=wordSet
-            tempWord=""
-            for j in range(n):
-                tempWord+=s[j]
-            if not tempWord in wordSet:
-                continue
+        l = len(s)
+        n = len(words[0])
+        m = len(words)
+
+        d = dict()
+        res = []
+
+        for word in words:
+            if word in d:
+                d[word] += 1
             else:
-                tempSet.remove(tempWord)
-                while tempSet:
+                d[word] = 1
 
+        for i in range(n):
+            left = i
+            right = i
+            crnt_cnt = dict()
+            count = 0
 
+            while right+n <= l:
+                w = s[right:right+n]
+                right += n
 
+                if w in d:
+                    if w in crnt_cnt:
+                        crnt_cnt[w] += 1
+                    else:
+                        crnt_cnt[w] = 1
+                    count += 1
+                    while crnt_cnt[w] > d[w]:
+                        left_w = s[left:left+n]
+                        crnt_cnt[left_w] -= 1
+                        count -= 1
+                        left += n
+
+                    if count == m:
+                        res.append(left)
+                else:
+                    crnt_cnt.clear()
+                    count = 0
+                    left = right
+
+        return res
 
 
 # @lc code=end
-
